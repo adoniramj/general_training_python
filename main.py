ed8910e1938a7d8030e704576efc39c49cfb1901
adoniramj <adoniramjvargas@gmail.com>
2020-02-07 20:07:16 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import json
+
+items = json.loads('[{"id" : "1", "text" : "Item1"},{"id" : "2", "text" : "Item2"}]')
+
+for item in items:
+  print(item['text'])
+
+for item in items:
+  print(item['id'])
